04383dec1dd6f03c462e4350142805f7eba0545a
XComatoseX <denismanko2006@gmail.com>
2024-02-27 16:40:36 +0200

    ☘️ Change name variable

--- js/elementWidth.js
+++ js/elementWidth.js
@@ -1,16 +1,13 @@
 const lineElement = document.getElementsByClassName('nav__list-item--line');
-
 const parentBlocks = document.querySelectorAll('.nav__list-item');
 const textEl = document.querySelectorAll(`.nav__list-item-link`)
 
 parentBlocks.forEach(function(parent) {
   parent.addEventListener('mouseover', function(event) {
-    const tt = document.getElementById(event.target.id)
-    if(!tt) return
-    console.log(tt.offsetWidth)
-    console.log((tt.offsetWidth - 20) / 2 + 'px')
+    const targetElement = document.getElementById(event.target.id)
+    if(!targetElement) return
     for (let i = 0; i < lineElement.length; i++) {
-      lineElement[i].style.left = (tt.offsetWidth - 20) / 2 + 'px';
+      lineElement[i].style.left = (targetElement.offsetWidth - 20) / 2 + 'px';
     }
   });
 });
